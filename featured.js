9081b5202c2ecd5489f178c56d96c9e908523ced
farestyk <76780667+farestyk@users.noreply.github.com>
2022-12-20 05:41:59 +0200

    throwing ideas

--- featured.js
+++ featured.js
@@ -7,25 +7,42 @@ function show1() {
         }
 }
 function filter() {
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 20; i++) {
         let men = document.getElementsByClassName('men')[i];
         let boys = document.getElementsByClassName('boys')[i];
         let girls = document.getElementsByClassName('girls')[i];
         let women = document.getElementsByClassName('women')[i];
-        let uni = document.getElementsByClassName('unisex')[i];
-
-
+        
+        
+        var filter = document.getElementById('filtersec');
+        var prod = document.getElementsByClassName('products')[0];
+        var uni = document.getElementById('unisex');
         let mencheck = document.getElementById('mencheck');
         let womencheck = document.getElementById('womencheck');
         let boyscheck = document.getElementById('boyscheck');
         let girlscheck = document.getElementById('girlscheck');
         if (womencheck.checked == true && mencheck.checked == false && boyscheck.checked == false && girlscheck.checked == false ) {
-            men.style.display = 'none';
+            women.parentElement.style.display = 'block';
+            women.style.display = 'block';            men.style.display = 'none';
             boys.style.display = 'none';
             girls.style.display = 'none';
             men.parentElement.style.display = 'none';
             boys.parentElement.style.display = 'none';
             girls.parentElement.style.display = 'none';
+            uni.style.display = 'block';
+            uni.parentElement.style.display = 'block';
+            // women.parentElement.style.position = 'absolute';
+            // women.parentElement.style.left = '12px';
+            /*prod.style.display = 'none';
+            filter.style.display = 'block';
+            const newdiv = document.createElement('div');
+            newdiv.appendChild(women);
+            newdiv.style.height = '200px';
+            newdiv.style.width = '20px';
+            newdiv.style.position = 'relative';
+            newdiv.style.left = '200px';
+            document.body.insertBefore(newdiv, filter); 
+            */
         }  else if(womencheck.checked == true && mencheck.checked == true && boyscheck.checked == false && girlscheck.checked == false ){
             women.parentElement.style.display = 'block';
             women.style.display = 'block';
@@ -35,7 +52,21 @@ function filter() {
             girls.style.display = 'none';
             boys.parentElement.style.display = 'none';
             girls.parentElement.style.display = 'none';
-        }  else if(womencheck.checked == false && mencheck.checked == false && boyscheck.checked == false && girlscheck.checked == false ){
+            uni.style.display = 'block';
+            uni.parentElement.style.display = 'block';
+        }
+          else if(womencheck.checked == true && mencheck.checked == true && boyscheck.checked == false && girlscheck.checked == true ){
+            women.parentElement.style.display = 'block';
+            women.style.display = 'block';
+            men.parentElement.style.display = 'block';
+            men.style.display = 'block';
+            boys.style.display = 'none';
+            girls.style.display = 'block';
+            boys.parentElement.style.display = 'none';
+            girls.parentElement.style.display = 'block';
+            uni.style.display = 'block';
+            uni.parentElement.style.display = 'block';
+        }else if (womencheck.checked == false && mencheck.checked == false && boyscheck.checked == false && girlscheck.checked == false) {
             women.parentElement.style.display = 'block';
             women.style.display = 'block';
             boys.style.display = 'block';
@@ -44,6 +75,8 @@ function filter() {
             girls.parentElement.style.display = 'block';
             men.parentElement.style.display = 'block';
             men.style.display = 'block';
+            uni.style.display = 'block';
+            uni.parentElement.style.display = 'block';
         }else if (mencheck.checked == true && womencheck.checked == false && boyscheck.checked == false && girlscheck.checked == false ) {
             women.parentElement.style.display = 'none';
             women.style.display = 'none';
@@ -53,16 +86,20 @@ function filter() {
             men.parentElement.style.display = 'block';
             boys.parentElement.style.display = 'none';
             girls.parentElement.style.display = 'none';
+            uni.style.display = 'block';
+            uni.parentElement.style.display = 'block';
         }
         else if (mencheck.checked == false && womencheck.checked == false && boyscheck.checked == true && girlscheck.checked == false ) {
-            women.parentElement.style.display = 'none';
             women.style.display = 'none';
-            men.style.display = 'none';
+            women.parentElement.style.display = 'none';
             boys.style.display = 'block';
-            girls.style.display = 'none';
-            men.parentElement.style.display = 'none';
             boys.parentElement.style.display = 'block';
+            girls.style.display = 'none';
             girls.parentElement.style.display = 'none';
+            men.style.display = 'none';
+            men.parentElement.style.display = 'none';
+            uni.style.display = 'none';
+            uni.parentElement.style.display = 'none';
         }
         else if (mencheck.checked == true && womencheck.checked == false && boyscheck.checked == true && girlscheck.checked == false ) {
             women.parentElement.style.display = 'none';
@@ -73,6 +110,8 @@ function filter() {
             men.parentElement.style.display = 'block';
             boys.parentElement.style.display = 'block';
             girls.parentElement.style.display = 'none';
+            uni.style.display = 'block';
+            uni.parentElement.style.display = 'block';
         }else if (mencheck.checked == false && womencheck.checked == true && boyscheck.checked == true && girlscheck.checked == false ) {
             women.parentElement.style.display = 'block';
             women.style.display = 'block';
@@ -82,6 +121,32 @@ function filter() {
             men.parentElement.style.display = 'none';
             boys.parentElement.style.display = 'block';
             girls.parentElement.style.display = 'none';
+            uni.style.display = 'block';
+            uni.parentElement.style.display = 'block';
+        }
+        else if (mencheck.checked == false && womencheck.checked == true && boyscheck.checked == true && girlscheck.checked == true ) {
+            women.parentElement.style.display = 'block';
+            women.style.display = 'block';
+            men.style.display = 'none';
+            boys.style.display = 'block';
+            girls.style.display = 'block';
+            girls.parentElement.style.display = 'block';
+            men.parentElement.style.display = 'none';
+            boys.parentElement.style.display = 'block';
+            uni.style.display = 'block';
+            uni.parentElement.style.display = 'block';
+        }
+        else if (mencheck.checked == true && womencheck.checked == false && boyscheck.checked == true && girlscheck.checked == true ) {
+            men.parentElement.style.display = 'block';
+            men.style.display = 'block';
+            women.style.display = 'none';
+            boys.style.display = 'block';
+            girls.style.display = 'block';
+            girls.parentElement.style.display = 'block';
+            women.parentElement.style.display = 'none';
+            boys.parentElement.style.display = 'block';
+            uni.style.display = 'block';
+            uni.parentElement.style.display = 'block';
         }
         else if (mencheck.checked == true && womencheck.checked == true && boyscheck.checked == true && girlscheck.checked == false ) {
             women.parentElement.style.display = 'block';
@@ -92,6 +157,8 @@ function filter() {
             men.parentElement.style.display = 'block';
             boys.parentElement.style.display = 'block';
             girls.parentElement.style.display = 'none';
+            uni.style.display = 'block';
+            uni.parentElement.style.display = 'block';
         }else if (mencheck.checked == false && womencheck.checked == false && boyscheck.checked == false && girlscheck.checked == true ) {
             women.parentElement.style.display = 'none';
             women.style.display = 'none';
@@ -101,6 +168,8 @@ function filter() {
             men.parentElement.style.display = 'none';
             boys.parentElement.style.display = 'none';
             girls.parentElement.style.display = 'block';
+            uni.style.display = 'none';
+            uni.parentElement.style.display = 'none';
         }else if (mencheck.checked == false && womencheck.checked == false && boyscheck.checked == true && girlscheck.checked == true ) {
             women.parentElement.style.display = 'none';
             women.style.display = 'none';
@@ -110,6 +179,8 @@ function filter() {
             men.parentElement.style.display = 'none';
             boys.parentElement.style.display = 'block';
             girls.parentElement.style.display = 'block';
+            uni.style.display = 'none';
+            uni.parentElement.style.display = 'none';
         }else if (mencheck.checked == true && womencheck.checked == false && boyscheck.checked == false && girlscheck.checked == true ) {
             women.parentElement.style.display = 'none';
             women.style.display = 'none';
@@ -128,6 +199,8 @@ function filter() {
             men.parentElement.style.display = 'none';
             boys.parentElement.style.display = 'none';
             girls.parentElement.style.display = 'block';
+            uni.style.display = 'block';
+            uni.parentElement.style.display = 'block';
         }else if (mencheck.checked == true && womencheck.checked == true && boyscheck.checked == true && girlscheck.checked == true ) {
             women.parentElement.style.display = 'block';
             women.style.display = 'block';
@@ -137,6 +210,25 @@ function filter() {
             men.parentElement.style.display = 'block';
             boys.parentElement.style.display = 'block';
             girls.parentElement.style.display = 'block';
+        }
+
+}
+}
+function filter2() {
+    for (var i = 0; i < 10; i++) {
+        let tops = document.getElementsByClassName('top')[i];
+        let bottoms = document.getElementById('bottoms')[i];
+        let legwear = document.getElementsByClassName('legwear')[i];
+
+        let topcheck = document.getElementById('topcheck');
+        let bottomcheck = document.getElementById('bottomcheck');
+        let legcheck = document.getElementById('legcheck');
+        if (topcheck.checked == true && bottomcheck.checked == false && legcheck.checked == false)  {
+            top.style.display = 'block';
+            bottoms.style.display = 'none';
+            legwear.style.display = 'none';
+            legwear.parentElement.style.display = 'none';
+            legwear.childElement
         }
 }
 }
