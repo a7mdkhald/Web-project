e03224b8d090c4cdf6e2072bda976d51e50ac230
omar957 <68370584+omar957@users.noreply.github.com>
2022-12-30 08:25:03 +0200

    p

---
+++ storey.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 30, 2022 at 07:23 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `storey`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phonenumber` int(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `email`, `phonenumber`, `password`, `address`) VALUES
+('omar', 'omarhashim5421@gmail.com', 1016816273, '123456as', 'alex'),
+('', '', 0, '', ''),
+('', '', 0, '', ''),
+('fares', 'fares@gmail.com', 1016816273, '123456as', 'alex'),
+('omar', 'omarhashim5421@gmail.com', 1016816273, '123456as', 'alex'),
+('', '', 0, '', ''),
+('fares', 'fares@gmail.com', 1016816273, '123456as', 'jdijdi'),
+('hfufh', 'mmcks@klk.com', 1017216273, '123456as', 'fhfhufh');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
